7fef5f48ae67394e113105f01f7503db466d2b36
pelumiolutimehin <63970555+pelumiolutimehin@users.noreply.github.com>
2020-06-17 22:27:56 -0500

    Add files via upload

---
+++ SsmuelOlutimehin_HelloWorldProgram/SsmuelOlutimehin_HelloWorldProgram/SamuelOlutimehin_HelloWorldProgram.cpp
@@ -0,0 +1,22 @@
+/*Oluwapelumi Olutimehin
+Computer Programming I
+29th January 2020
+
+Program Summary:
+This program displays statements
+*/
+//--- Start here
+//identifying the header/preprocessor files
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+
+	//outputting information to the screen
+	cout << "Hello world! This is my first computer program!" << endl;
+
+	return 0;    //exit th program
+}
+//--- End here
---
+++ SsmuelOlutimehin_HelloWorldProgram/SsmuelOlutimehin_HelloWorldProgram/SamuelOlutimehin_PrgAssignmentI.cpp
@@ -0,0 +1,15 @@
+//--- Start here
+//identifying the header/preprocessor files
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+
+	//outputting information to the screen
+	cout << "Hello world! This is my first computer program!" << endl;
+
+	return 0;    //exit th program
+}
+//--- End here
